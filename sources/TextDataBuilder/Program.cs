bcbc49bf0ba86d40e8601cfa7769da3123548aa1
Cedric VERNOU <cedric.vernou@gmail.com>
2019-10-30 22:44:26 +0100

    #1 Initialize the solution

---
+++ sources/TextDataBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextDataBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
